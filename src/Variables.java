1539537222e82a8c2a290913054baf2daac386e5
sergioalumnodevops <servillanez@hotmail.com>
2023-10-16 17:42:15 +0200

    Initial commit

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Variables {
+import java.util.Scanner;
+
+public class Variables {
+    public static void main (String[] args){
+        System.out.println("Hola Mundo");
+        Scanner tecladoEntrada = new Scanner(System.in);
+        System.out.println("Escriba su edad");
+        int edad =0;
+        edad= tecladoEntrada.nextInt();
+        System.out.println("El dato leido es-->"+edad);
+
+        //Decir si es mayor o menor de edad
+
+        if (edad>=18) {
+            System.out.println("Mayor de edad");
+        }else {
+            System.out.println("Menor de edad");
+
+        }
+
+
+
+    }
 }
